f39533d69ff79d7b555d41caa21f447b673798d2
Zach Stone <ga83kij@mytum.de>
2017-01-14 23:47:35 +0100

    Added Send Notification button + placeholder code

--- Backend/atse_project/src/main/java/com/atse/group_2/TutorView.java
+++ Backend/atse_project/src/main/java/com/atse/group_2/TutorView.java
@@ -17,36 +17,47 @@ public class TutorView extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 		      throws IOException {
-		response.getWriter().println("<h1>Welcome to the tutor page.</h1>");
 		response.setContentType("text/html");
-	      response.getWriter().println("<html><head><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css\" "
+		response.getWriter().println("<h1>Welcome to the tutor page.</h1>");
+		
+	    response.getWriter().println("<html><head><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css\" "
 	      		+ "integrity=\"sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi\" crossorigin=\"anonymous\"></head><body>");
-	      String bonusMessage = (String) request.getSession().getAttribute("bonusMessage");
-			if (bonusMessage != null) {
-				response.getWriter().println("<div class=\"alert alert-success\">" + bonusMessage + "</div>");
-				HttpSession session = request.getSession();
-				session.setAttribute("bonusMessage", null);
-			}
-	      response.getWriter().println("<form action=\"tutorview\" method=\"POST\"><table class=\"table\"><thead class=\"thead\">"+
-"<tr><th>Group</th><th>Student</th><th>Presence</th><th>Presentation</th></tr></thead><tbody>");
+	      
+	    String bonusMessage = (String) request.getSession().getAttribute("bonusMessage");
+			
+	    if (bonusMessage != null) {
+	    	response.getWriter().println("<div class=\"alert alert-success\">" + bonusMessage + "</div>");
+			HttpSession session = request.getSession();
+			session.setAttribute("bonusMessage", null);
+		}
+	    
+	    String notificationMessage = (String) request.getSession().getAttribute("notificationMessage");
+	    
+	    if (notificationMessage != null){
+	    	response.getWriter().println("<div class=\"alert alert-success\">" + notificationMessage + "</div>");
+			HttpSession session = request.getSession();
+			session.setAttribute("notificationMessage", null);
+	    }
+			
+	    response.getWriter().println("<form action=\"tutorview\" method=\"POST\"><table class=\"table\"><thead class=\"thead\">"+
+	    		"<tr><th>Group</th><th>Student</th><th>Presence</th><th>Presentation</th></tr></thead><tbody>");
 	      
 		      
-		      String username = (String)request.getSession().getAttribute("username");
-		      Person tutor = ObjectifyService.ofy().load().type(Person.class).id(username).now();
+		String username = (String)request.getSession().getAttribute("username");
+		Person tutor = ObjectifyService.ofy().load().type(Person.class).id(username).now();
 		      
-		      // This implementation gets everybody in the database and compares their groups and roles manually. 
-		      // Should be rewritten to be more efficient. Example:
-		      // List<Car> cars = ofy().load().type(Car.class).filter("year >", 1999).list()
-		      List<Person> students = ObjectifyService.ofy().load().type(Person.class).list();
-		      Iterator<Person> it = students.iterator();
+		// This implementation gets everybody in the database and compares their groups and roles manually. 
+		// Should be rewritten to be more efficient. Example:
+		// List<Car> cars = ofy().load().type(Car.class).filter("year >", 1999).list()
+		List<Person> students = ObjectifyService.ofy().load().type(Person.class).list();
+		Iterator<Person> it = students.iterator();
 		      
-		      while(it.hasNext())
-		      {		    	  
-		      	Person student = it.next();
+		while(it.hasNext()) {		    	  
+		    Person student = it.next();
 		      	
-		      	if(student.group.equals(tutor.group) && student.role == Person.Roles.STUDENT.getValue()) {
-			      	Group g = ObjectifyService.ofy().load().type(Group.class).id(tutor.group).now();
-		      		response.getWriter().println(
+		    if(student.group.equals(tutor.group) && student.role == Person.Roles.STUDENT.getValue()) {
+		    	Group g = ObjectifyService.ofy().load().type(Group.class).id(tutor.group).now();
+		      	response.getWriter().println(
 			      			"<tr><td>" 
 			      			+ student.group 
 		      				+ "</td><td>" 
@@ -57,14 +68,29 @@ public class TutorView extends HttpServlet {
 			      			+ "</td><td>" 
 			      			+ (student.attendance == null ? "false" : student.attendance.containsValue(Person.Scores.PARTICIPANT)) 
 			      			+ "</td></tr>");
-		      	}
-		      }     
+		    }
+		}     
 		      
-	      response.getWriter().println("</tbody></table><button style=\"margin-right: 5px;\"type=\"submit\" class=\"btn btn-primary\">Calculate bonuses</button>"
-	      		+ "<a href=\"login\" class=\"btn btn-primary\">Log out</a></form></body>");		
+	    response.getWriter().println("</tbody></table><button style=\"margin-right: 5px;\"type=\"submit\" class=\"btn btn-primary\">Calculate Bonuses</button>"
+	      	 // + "<a href=\"tutorview/send\" style=\"margin-right: 5px;\" class=\"btn btn-primary\">Send Bonus Notifications</a>"
+	    		+ "<button style=\"margin-right: 5px;\"type=\"submit\" formaction=/tutorview/send class=\"btn btn-primary\">Send Bonus Notifications</button>"
+	    	+ "<a href=\"login\" class=\"btn btn-primary\">Log Out</a></form></body>");	
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {		
+		String pathInfo = request.getPathInfo();
+		pathInfo = pathInfo == null ? "" : pathInfo;
+		
+		switch (pathInfo){
+		case "/send":	
+			handleNotifications(request, response);
+			break;
+		default:
+			handleBonusMessage(request, response);
+		}		
+	}
+	
+	private void handleBonusMessage(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		String message 			= "There are no students eligible for the bonus.";
 		String username 		= (String)request.getSession().getAttribute("username");
 	    Person tutor 			= ObjectifyService.ofy().load().type(Person.class).id(username).now();
@@ -81,6 +107,21 @@ public class TutorView extends HttpServlet {
 	    
 	    HttpSession session = request.getSession();
 	    session.setAttribute("bonusMessage", message);
-		response.sendRedirect("/tutorview");		
+	    session.setAttribute("notificationMessage", null);
+		response.sendRedirect("/tutorview");
 	}
+	
+	private void handleNotifications(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		String message 			= "Bonus notifications have been sent.";
+		
+		// Hmm.... how to actually send notifications? Email? Push to android?
+		// For Backend: https://firebase.google.com/docs/cloud-messaging/ and https://firebase.google.com/docs/cloud-messaging/send-message  formerly https://developers.google.com/cloud-messaging/http
+		// For Android clients: https://developers.google.com/cloud-messaging/android/client
+		
+	    HttpSession session = request.getSession();
+	    session.setAttribute("notificationMessage", message);
+	    session.setAttribute("bonusMessage", null);
+		response.sendRedirect("/tutorview");
+	}
+	
 }
